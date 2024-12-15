4fc5e5a81ea0a05f815dc85c46c22f9535bf6863
Olaf Cichosz <01171645@pw.edu.pl>
2024-12-15 21:00:35 +0100

    zad1

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -1,8 +1,28 @@
 #include "catch.hpp"
+#pragma once
 
 // Zad2
 // tutaj definicja klasy Makaron
 // BEZ definicji metody gotujMakaron (tylko z deklaracją)
 
-// Zad1
-// tutaj definicja klasy Tagliatelle
+
+
+class Tagliatelle
+{
+private:
+    double L; // długość kawałka makaronu
+    double W; // szerokość kawałka makaronu
+    double R; // proporcja jajek do mąki
+
+    static const double C; // stała zadeklarowana, ale nie zdefiniowana tutaj
+
+public:
+    // Konstruktor domyślny
+    Tagliatelle() : L(0.5), W(0.5), R(0.5) {}
+
+    // Konstruktor parametryczny
+    Tagliatelle(double length, double width, double ratio) : L(length), W(width), R(ratio) {}
+
+    // Metoda ileMaki zwracająca masę mąki dla P porcji
+    double ileMaki(unsigned P) const { return P * L * W * (1.0 - R) * C; }
+};
